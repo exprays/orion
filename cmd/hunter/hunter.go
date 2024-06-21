fac39107657f7c4e56f0f9f424152f1a86e5e4b2
Surya <suryakant.subudhi@outlook.com>
2024-06-21 21:30:54 +0530

    update hunter

--- cmd/hunter/hunter.go
+++ cmd/hunter/hunter.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"orion/src/hunter" // Import the hunter package
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: hunter connect")
-		os.Exit(1)
-	}
-
-	command := os.Args[1]
-
-	switch command {
-	case "connect":
-		hunter.Connect()
-	default:
-		fmt.Println("Unknown command. Available commands: connect")
-	}
+	// Call the hunter.Connect function directly
+	hunter.Connect()
 }
